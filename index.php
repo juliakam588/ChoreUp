bb1aafbca300b5d1a7484b6e3d2a5b53f5ba963a
Julia Kamuda <julka.kamuda@gmail.com>
2023-12-03 18:43:23 +0100

    added routing and upddated index

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
 
-echo 'Hi there 👋';
+require 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url( $path, PHP_URL_PATH);
+
+
+Routing::get('', 'DefaultController');
+Routing::get('login', 'DefaultController');
+Routing::get('registration', 'DefaultController');
+Routing::get('afterRegistration', 'DefaultController');
+Routing::get('dashboard', 'DefaultController');
+Routing::get('FileNotFound', 'ErrorController');
+Routing::get('household', 'DefaultController');
+Routing::post('login', 'SecurityController');
+
+Routing::run($path);
